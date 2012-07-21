5ff62f4a3e88577048ec387adcf2be46e52ddbbb
Maksim Muruev <maksim.muruev@gmail.com>
2012-07-21 16:22:13 +0300

    - snaphot

--- src/main/java/edisoft/parser/DataLoader.java
+++ src/main/java/edisoft/parser/DataLoader.java
@@ -1,19 +1,21 @@
 package edisoft.parser;
 
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.*;
 import javax.xml.transform.sax.SAXTransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import java.io.InputStream;
 
 public class DataLoader {
 
-    private static Templates templates;
+    private static  Templates templates;
 
-    public DataLoader(String xsltFilePath) {
-        initTemplate(xsltFilePath);
-    }
-
-    private void initTemplate(String xsltFilePath) {
+    public DataLoader(String xsltFilePath){
         TransformerFactory tf = TransformerFactory.newInstance();
         if (!tf.getFeature(SAXTransformerFactory.FEATURE)) {
             throw new RuntimeException(
@@ -30,12 +32,18 @@ public class DataLoader {
         }
     }
 
-
     public StreamResult transformer(StreamSource sourceStream) throws TransformerException {
         Transformer t = templates.newTransformer();
         StreamResult xmlResult = new StreamResult();
         t.transform(sourceStream, xmlResult);
         return xmlResult;
     }
+
+    public Document parse(InputStream xmlInputStream) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        return builder.parse(xmlInputStream);
+    }
+
 
 }
